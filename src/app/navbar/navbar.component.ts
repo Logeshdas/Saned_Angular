e27e38b417d1241fce41dd147622ccf7beff8698
Arjunan95 <arjun.sankar@rapidqube.com>
2019-04-21 21:08:43 +0530

    changes in register button while clicking going to eservice.spsa.ae/register_en

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core';
+// import router from "@angular/router";
+//import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -9,12 +10,19 @@ export class NavbarComponent implements OnInit {
 
   navbarOpen = false;
 
-    toggleNavbar() {
-        this.navbarOpen = !this.navbarOpen;
-    }
-    localVar;
-  constructor() { }
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+  // goSearch() {
+  //   this.router.navigate(['search']); (3)
+  // }
+  // goToUrl(): void {
+  //   this.document.location.href = 'http://eservices.spsa.ae';
+  // }
+  localVar;
 
+  constructor() { }
+  // @Inject(DOCUMENT)
   ngOnInit() {
   }
 
